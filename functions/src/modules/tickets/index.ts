0c1e8ac9235b090f1682737ff69d31e675e119c6
Jesal Pande <pandejesal@gmail.com>
2026-09-11 15:25:45 +0530

    Loop 13: remove dead ticket escalation/reminder workers (superseded, zero refs)

--- functions/src/modules/tickets/index.ts
+++ functions/src/modules/tickets/index.ts
@@ -1,5 +1,3 @@
 export * from "./tickets.service";
 export * from "./ticketReminder.scheduler";
-export * from "./escalationScheduler";
-export * from "./reminderCron";
 export * from "./notificationDispatcher";
